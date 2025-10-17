435965c65ed0eab77bb520f4d99e4657071bdfe1
DanillodaSilva <dssiqueira2021@gmail.com>
2025-10-16 22:49:17 -0300

    aprendendo c

---
+++ aprendendo/primeira-aula.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+
+int main () {
+  /* 
+  int é o numero inteiro (%d)
+  float numero com virgula (%f)
+  char só tem a opção de ocupar 1 caractere (%c)
+  string recebe texto (%s)
+  */ 
+  
+
+ int idade;
+ idade = "18";
+ //da forma abaixo q se reproduz algo
+printf ("A sua idade é: %d/n", idade);
+return 0;
+
+}
+
+
+
+
